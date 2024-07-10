fd459e923fe31089f2d1e65f92bdaa92a6f0642e
Olaoluwa  Ayidina <ayidinao@gmail.com>
2024-07-10 15:20:10 +0100

    Create OrderList.jsx
    updated: Added the orderlist

---
+++ src/components/orderList/OrderList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const OrderList = ({ orders, markAsDelivered, setSelectedOrder }) => {
+  return (
+    <div>
+      <h2>Order List</h2>
+      {orders.length === 0 ? (
+        <p>No orders available.</p>
+      ) : (
+        <ul>
+          {orders.map((order, index) => (
+            <li key={index}>
+              <div>
+                <p>Order #{index + 1}</p>
+                <p>Food: {order.name}</p>
+                <p>Price: ${order.price}</p>
+                <p>Status: {order.status}</p>
+                <button onClick={() => markAsDelivered(index)}>
+                  Mark as Delivered
+                </button>
+                <button onClick={() => setSelectedOrder(order)}>
+                  View Details
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>)
+  );
+};
+
+export default OrderList;
