db3f939829111410d6d0097c74318af685757d65
Olaoluwa  Ayidina <ayidinao@gmail.com>
2024-07-10 15:26:48 +0100

    Create VerifyEmail.jsx
    updated: added email

---
+++ src/components/verifyEmail/VerifyEmail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VerifyEmail = () => {
+  return (
+    <div>
+      <h2>Verify Your Email</h2>
+      <p>Please check your email and click the verification link to verify your account.</p>
+    </div>
+  );
+};
+
+export default VerifyEmail;
